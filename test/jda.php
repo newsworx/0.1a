4c15c0fb8f6a5c855b223071d2aec3ea9245e5f6
Juan De Anda <familiadeanda@gmail.com>
2013-04-11 13:06:02 -0700

    added jda file to test directory

---
+++ test/jda.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+
+
+
+<a data-pin-config="above" href="//pinterest.com/pin/create/button/" data-pin-do="buttonBookmark" ><img src="//assets.pinterest.com/images/pidgets/pin_it_button.png" /></a>
+
+
+</body>
+</html>
