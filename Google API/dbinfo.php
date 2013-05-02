a0d1985ce0e17feddcb65619cab29031cd581de2
misclet <k.redublo@gmail.com>
2013-05-02 13:28:49 -0700

    dbinfo

---
+++ Google API/dbinfo.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8" />
+
+<title>Database access</title>  
+
+</head>  
+
+<body>
+
+<?php
+
+$username = "jfolio";
+$password = "$fsU2013";
+$database = "jfolio-memories";
+
+?>
+
+</body>
+
+</html>
