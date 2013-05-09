c34f64c77052c723e416c72efd6124dfd2eb2c7c
misclet <k.redublo@gmail.com>
2013-05-09 12:08:17 -0700

    API

---
+++ Google API/A08_API_team_doc.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+
+<html xmlns:fb="http://ogp.me/ns/fb#">
+
+<head>
+
+<meta charset="utf-8" />
+
+<title>API Team Doc</title>  
+
+</head>  
+
+
+
+<body>
+
+<!--Facebook button-->
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=238363846301972";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<fb:like href="http://newsworxapps.com/j680/tjh/A08-API/A08_Like_Button_tjh.php" send="true" width="450" show_faces="true" colorscheme="dark"></fb:like>
+
+
+<!--LinkedIn button-->
+
+<script src="//platform.linkedin.com/in.js" type="text/javascript">
+</script>
+<script type="IN/Share" data-counter="top"></script>
+
+<!--Twitter button-->
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Tweet Button </title>
+</head>
+
+<!--Pintrist-->
+
+<a href="http://pinterest.com/pin/create/button/?url=http%3A%2F%2Fwww.flickr.com%2Fphotos%2Fkentbrew%2F6851755809%2F&media=http%3A%2F%2Ffarm8.staticflickr.com%2F7027%2F6851755809_df5b2051c9_z.jpg&description=Next%20stop%3A%20Pinterest" data-pin-do="buttonPin" data-pin-config="above"><img src="http://assets.pinterest.com/images/pidgets/pin_it_button.png"/></a>
+
+<body>
+
+<a href="https://twitter.com/share?<?php echo $current_url; ?>"  class="twitter-share-button" data-lang="es">Tweet</a>
+
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+
+
+
+</body>
+
+</html>
