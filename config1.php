0f2a2f1762b8fcd909102795365cebe8499406e9
Adrian Rodriguez <adrianrrodri1@gmail.com>
2013-05-16 13:56:28 -0700

    data retrieval login

---
+++ config1.php
@@ -0,0 +1,10 @@
+<?php
+
+$con = mysql_connect("localhost","jfolio_juser","j2013","jfolio_j680") or die(mysql_error());
+        mysql_select_db("database_name") or die(mysql_error());
+        if($con)
+        {
+            echo "connected";
+        }
+        
+ ?>       
