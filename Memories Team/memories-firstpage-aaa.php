a1dd6bb41fa38419b6d19fb4cf1856045e2bf3f2
Journalism Student <journalism@nhum-j169.sfsu.edu>
2013-05-17 14:37:25 -0700

    Add New Memory Page
    Three pages for add new memory

---
+++ Memories Team/memories-firstpage-aaa.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+
+<body> 
+
+<h1>  Memory Lane  </h1>
+
+<head>
+
+
+<title>  </title>
+
+</head>
+
+<body>
+
+<ol>
+<a href ="http://newsworxapps.com/j680/aaa/A07/memories-secondpage-aaa.php?>=Add New Memory">Add New Memory</a> 
+</ol>
+
+</body>
+
+</html>
+
---
+++ Memories Team/memories-secondpage-aaa.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+<meta charset="utf-8" />
+
+<h1><center> Add a memory!</center></h1>
+
+<center>
+
+</head>
+
+<body>
+
+<br>
+
+<hr />
+
+<center>
+
+<p />
+
+<form name="add memory" method="get" action="aaa_third_page.php">
+<center>
+
+Memory Label: <input name="mem_label" />
+<br>
+Memory Name: <input name="mem_name"  />
+<br>
+Latitude: <input name="latitude" />
+<br>
+Longitude: <input name="longitude" />
+<br>
+Memory Description: <input name="mem_description" />
+</center>
+<p />
+
+<input type="submit" value="Submit" />
+
+</form>
+
+
+</center>
+
+</body>
+
+</html>
