39e483df6e271650ca762919dac37f27c800b42a
Jeff Sandstoe <Jsandstoe@gmail.com>
2013-05-21 11:24:43 -0700

    Created new pages
    Working on getting the create a new memory lane feature up and running,

---
+++ 0.2a/new_lane.php
@@ -0,0 +1,102 @@
+<?php
+
+require 'constants.php';
+
+// detect authentication cookie. if logged in, proceed.
+// if not, kick user back to login screen
+
+if ($_COOKIE['compass_auth'] == '')
+
+{
+
+	header('Location: login.php');
+
+}
+
+// grab user number from saved cookie
+
+$uid = $_COOKIE['compass_auth'];
+
+// query for user details
+
+$query = "SELECT * FROM users WHERE user_num = " . $uid;
+
+$result = mysqli_query($db_connection,$query);
+
+while ($row = mysqli_fetch_array($result))
+
+{
+
+	// assign user details to php variables
+
+	$first_name 	= $row["first_name"];
+	$last_name 		= $row["last_name"];
+	$user_id 		= $row["user_id"];
+
+}
+
+?>
+<!DOCTYPE html>
+
+<html> 
+
+<head> 
+
+<title>Memory Lane - Default screen</title> 
+
+<meta name="viewport" content="width=device-width, initial-scale=1"> 
+
+<link rel="stylesheet" href="c/compass0-2.min.css" />
+<link rel="stylesheet" href="c/ml.css" />
+<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.1/jquery.mobile.structure-1.3.1.min.css" />
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.3.1/jquery.mobile-1.3.1.min.js"></script>
+
+<link rel="stylesheet" href="c/ml.css" />
+
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=true"></script>
+    
+</head>
+ 
+<body>  
+
+<div data-role="page">
+
+<div data-role="header">
+
+<?php echo "<h4>Create your new memory lane, " . $first_name . "!</h4>"; ?>
+
+	</div><!-- /header -->
+    
+<div data-role="content">
+
+<form action="create_newlane.php" method="post">
+     New lane name:</label>
+     <input type="text" name="new-name" id="new-name" value="" placeholder="Name your new memory lane here...">
+     
+<br>
+ <br>
+Description of new lane :
+    <input type="text" textarea cols="40" rows="8" name="description" id="description" placeholder="Please provide a description of your new memory lane."></textarea>
+
+
+<br>
+<br>
+    <input type="submit" value="Create Memory Lane">
+    </form>
+
+</div><!-- content -->
+    
+    <div data-role="footer" data-position="fixed" data-theme="c">
+ 
+<span class="ui-title" />
+<a href="edit_profile.php"  class="ui-btn-left"  data-theme="a" data-mini="true" data-icon="gear" data-role="button" data-transition="pop">Edit Profile</a>
+<a href="signout.php" data-theme="a" class="ui-btn-right" data-mini="true" data-icon="delete" data-role="button" data-transition="pop">Logout</a>
+
+
+
+	</div><!-- /footer -->
+
+</body>
+
+</html>
